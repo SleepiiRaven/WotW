cef656d1d71d6485aa48a8827d6d6faca8e39724
SleepiiRaven <sleepingravenytb@gmail.com>
2022-07-13 15:21:03 -0700

    Update

--- Willows of the Wild/GUI/TitleScreen/TitleScreen.gd
+++ Willows of the Wild/GUI/TitleScreen/TitleScreen.gd
@@ -1,7 +1,9 @@
 extends Control
 
+var saved_scene
+
 func _on_Button_pressed():
-	get_tree().change_scene("res://World.tscn")
+	saved_scene = get_tree().change_scene("res://World.tscn")
 
 func _on_Button2_pressed():
 	get_tree().quit()
